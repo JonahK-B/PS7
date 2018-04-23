b58d5efb25400c259f972070e4bb0b1bc077795e
JonahK-B <36051322+JonahK-B@users.noreply.github.com>
2018-04-23 15:29:40 -0500

    Part 4 done
    Waiting to ask a question on how part 5 should be grouped in class tmr

--- PS7.R
+++ PS7.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 
 library(readr)
+library(stringr)
 March2018 <- read_csv("~/GitHub/PS7/March2018.CSV")
 detach("package:EBMAforecast", unload = TRUE)
 detach("package:plyr", unload=TRUE)
@@ -54,5 +55,45 @@ arrange(crimeNHFreq1, desc(count)) ## NH 35: 305 crimes, NH 36: 203 crimes, NH 1
 
 
 #4
+districtCrime <- select(March2018, District, Description)
+districtCrime <- as_tibble(districtCrime)
 
 
+districtRobbery <- districtCrime %>%
+filter(str_detect(Description, "ROBBERY")) %>%
+  group_by(District) %>%
+  summarise(countRobbery = n())
+
+districtTotalCrime <- districtCrime %>%
+  group_by(District) %>%
+  summarise(countTotalCrime = n())
+
+robberyProportion <- right_join(districtRobbery, districtTotalCrime)
+robberyProportion[1,2] <- 0
+robberyProportion$robProp <- robberyProportion$countRobbery/robberyProportion$countTotalCrime
+
+arrange(robberyProportion, desc(robProp)) ## District 5 has the highest percentage of robberies, at ~4%
+
+
+#5
+library(ggplot2)
+
+crimeTypeByDay <- select(March2018, DateOccur, Description)## subsetting data set for day and type of crime
+##I set up a new table for any modified information rather than directly changing the original so I have the original for reference in case I accidently change anything.
+crimeTypeByDay <- as_tibble(crimeTypeByDay)
+
+crimeTypeByDay$DateOccur <- substr(crimeTypeByDay$DateOccur, 1,10)  ##Isolating date, removing timestamp
+crimeTypeByDay$Description <- strsplit(as.character(crimeTypeByDay$Description), "-") ## Generalizing types of crimes
+crimeTypeByDay$Description <- sapply(crimeTypeByDay$Description, function(x) x[1])
+
+
+##Counting number of different types of crimes by day
+## (some of these crimes happened before March 2018, but they are all reported in March)
+crimeFreq <- crimeTypeByDay %>%
+  group_by(Description, DateOccur) %>%
+  dplyr::summarise(count = n()) ##Specifying dplyr because there is a conflict with plyr
+
+
+
+
+  
